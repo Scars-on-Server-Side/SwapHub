d1b581fb0e4606605429bf0eff26cbc5fc89d2b3
Anastasia <anastasiadontsu21gmail.com>
2024-03-18 12:51:18 +0200

    add menu

--- frontend/src/pages/components/header/Header.tsx
+++ frontend/src/pages/components/header/Header.tsx
@@ -1,19 +1,64 @@
-import React from "react";
-
-function Header(props) {
-
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import { relative } from "path";
+export default function Header(props) {
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
   const handleClickAvatar = () => {
-    props.onClickAvatar()
-  }
+    props.onClickAvatar();
+  };
 
   return (
-    <header className="Header">
-      <h1 className="Swap">
-        Swap<span className="Hub">Hub</span>
-      </h1>
-      <img src="http://localhost:80/media/user.png" alt="avatar" onClick={handleClickAvatar} />
-    </header>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Swap<span style={{ color: "blue" }}>Hub</span>
+          </Typography>
+          <Button
+            id="basic-button"
+            aria-controls={open ? "basic-menu" : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? "true" : undefined}
+            onClick={handleClick}
+            sx={{ color: "#212121" }}
+          >
+            Login
+          </Button>
+          <Menu
+            id="basic-menu"
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            MenuListProps={{
+              "aria-labelledby": "basic-button",
+            }}
+          >
+            <MenuItem
+              onClick={() => {
+                handleClose();
+                handleClickAvatar();
+              }}
+            >
+              Login
+            </MenuItem>
+            <MenuItem onClick={handleClose}>Register</MenuItem>
+            <MenuItem onClick={handleClose}>My account</MenuItem>
+          </Menu>
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 }
-
-export default Header;
